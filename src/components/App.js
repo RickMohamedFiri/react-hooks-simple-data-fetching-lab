5d7f16a7e7443ff79922e174040260935f507e89
Rick Mohamed <rickmohaa@gmail.com>
2023-07-22 22:06:33 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React from 'react';
+import { useState, useEffect } from "react";
+
+const App = () => {
+    const [data, setData] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setIsLoaded(false);
+        console.log(data);
+      });
+  }, []);
+
+  // if the data hasn't been loaded, show a loading indicator
+  if (isLoaded) return <h3>Loading...</h3>;
+
+  return (data.map(dog => <img src={dog.img}></img>));
+}
+ 
+export default App;
